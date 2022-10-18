40c2f484811f33291599c6cab2e88760e8841cf6
Fatma <49373297+fatmakala@users.noreply.github.com>
2022-10-18 15:15:31 +0300

    Add files via upload

---
+++ polymorphismDemo/src/polymorphismDemo/CustomerManager.java
@@ -0,0 +1,18 @@
+package polymorphismDemo;
+
+public class CustomerManager {
+	private BaseLogger logger;
+	
+	public CustomerManager(BaseLogger logger)
+	{
+		this.logger=logger;
+	}
+	public void add() {
+	
+		System.out.println("Müþteri eklendi.");
+		/*DatabaseLogger logger = new DatabaseLogger();
+		logger.Log("Log mesajý");*/
+		this.logger.log("Log mesajý");
+	}
+
+}
